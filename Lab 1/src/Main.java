5f84c6abe1ffecd820944d6b890f927162734c16
Danik213 <152310107+Danik213@users.noreply.github.com>
2025-09-25 21:59:26 +0300

    fix merge

--- Lab 1/src/Main.java
+++ Lab 1/src/Main.java
@@ -1,32 +1,44 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.print("Введіть кількість чисел Фібоначі:");
-        int N = console.nextInt();
-        int[] arr = new int[N];
-        arr[0]=1;
-        arr[1]=1;
-        for (int i=2;i<N;i++){
-          arr[i]=arr[i-1]+arr[i-2];
-        }
-        System.out.println("Числа Фібоначі з заданою кількістю N");
-        for (int j=0;j<N;j++){
-            System.out.print(arr[j]+" ");
-        }
-        System.out.println("");
-        System.out.println("Числа Фібоначі з заданою кількістю N які є квадратами");
-        for (int k=0;k<N;k++){
-            boolean b = IsSquare(arr[k]);
-            if(b){
-                System.out.print(arr[k]+" ");
-            }
-
-        }
+        int N = readInput();
+        int[] arr = fibonacci(N);
+        System.out.println("Числа Фібоначі з заданою кількістю N:");
+        printArr(arr);
+        System.out.println("Числа Фібоначі, які є квадратами:");
+        printSquares(arr);
     }
-    public static boolean IsSquare(int num) {
-        if (num<0) return false;
-        int x=(int)Math.sqrt(num);
-        return x*x==num;
+    public static int readInput() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введіть кількість чисел Фібоначі: ");
+        return console.nextInt();
+    }
+    public static int[] fibonacci(int N) {
+        int[] arr = new int[N];
+        if (N > 0) arr[0] = 1;
+        if (N > 1) arr[1] = 1;
+        for (int i = 2; i < N; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr;
+    }
+    public static void printArr(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    public static void printSquares(int[] arr) {
+        for (int num : arr) {
+            if (isSquare(num)) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+    public static boolean isSquare(int num) {
+        if (num < 0) return false;
+        int x = (int) Math.sqrt(num);
+        return x * x == num;
     }
 }
